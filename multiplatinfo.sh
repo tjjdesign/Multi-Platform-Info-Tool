f38024af38da26ce79e5823a0a231291f60ee9f8
Thomas James <tjjdesign@outlook.com>
2020-01-10 22:24:40 -0700

    Basic Bash Script
    A working bash script that just gets hostname and IP addresses. Pushed to an output file and also clbin.

---
+++ multiplatinfo.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+#hostname function
+function get_hostname (){
+    printf "System Hostname:" & hostname & printf "\n"
+}
+
+#IP address function
+function get_IP_addresses () {
+    printf "\nIP Addresses: \n(Distinct interfaces will be on their own line.)\n" \
+    & ip addr | grep -E "(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)" | grep -v 'scope|global|static|dynamic'
+}
+
+#adds timestamp, runs functions, outputs to system.info, then cats file to clbin
+(date & printf "\n") >> system.info
+get_hostname >> system.info
+get_IP_addresses >> system.info
+cat system.info | curl -F 'clbin=<-' https://clbin.com
+
+#notify user that file has also been saved locally for their own use
+printf "Output also saved to system.info\n"
