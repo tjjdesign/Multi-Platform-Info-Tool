6334b7a9736fae7858a5f527e85ed0f46d0ddda1
Thomas James <tjjdesign@outlook.com>
2020-01-10 22:25:06 -0700

    Placeholder powershell file added
    Make this later

---
+++ multiplatinfo.ps1
@@ -0,0 +1,3 @@
+#Basic powershell script. Start with working it for just windows systems.
+#Ideally this could work on linux as well, since powershell core will install with "apt-get install -y powershell"
+#Right now this is a placeholder.
