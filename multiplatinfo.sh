ce10118f38aea41e45fadf4801f5824383213fac
Thomas James <tjjdesign@outlook.com>
2020-01-14 16:35:27 -0700

    Added installed program, services functions

--- multiplatinfo.sh
+++ multiplatinfo.sh
@@ -8,7 +8,7 @@ function get_hostname (){
 
 #IP address function
 function get_IP_addresses () {
-    printf "\nIP Addresses: \n(Distinct interfaces will be on their own line.)\n" \
+    printf "IP Addresses: \n(Distinct interfaces will be on their own line.)\n" \
     & ip addr | grep -E "(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)" | grep -v 'scope|global|static|dynamic'
     sleep 1
 }
@@ -19,21 +19,50 @@ function network-outgoing () {
 }
 
 function get-users () {
+    printf "Users:\n(Distinct users will be on their own line.)\n"
     cut -d: -f1 /etc/passwd
+    sleep 1
+}
+
+function get-installed () {
+    printf "Installed Programs:\n"
+    #if centos: rpm -qa
+    printf "RPM output: (Indicates CentOS/RHEL)\n"
+    rpm -qa
+    #if ubuntu/debian: apt-mark showmanual
+    printf "\nAPT output: (Indicates Debian/Ubuntu)\n"
+    apt-mark showmanual
+    sleep 1
+}
+
+function services () {
+    printf "Running/enabled services:\n"
+    systemctl list-unit-files | grep enabled
     sleep 1
 }
 
 #adds timestamp, runs functions, outputs to system.info, then cats file to clbin
 (date & printf "\n") >> system.info
+
 get_hostname >> system.info
 printf "\n" >> system.info
-#sleep 1
+
 get_IP_addresses >> system.info
 printf "\n" >> system.info
+
 network-outgoing >> system.info
 printf "\n" >> system.info
+
 get-users >> system.info
 printf "\n" >> system.info
+
+get-installed >> system.info
+printf "\n" >> system.info
+
+services >> system.info
+printf "\n" >> system.info
+
+#feed to clbin
 cat system.info | curl -F 'clbin=<-' https://clbin.com
 
 #notify user that file has also been saved locally for their own use
